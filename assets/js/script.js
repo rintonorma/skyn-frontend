5476606c1316f069be74fb99e2c16cb4a257fa11
rintonorma <rinto456@gmail.com>
2022-04-28 10:26:14 +0700

    homepage

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+  $('.testimonials_carousel').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    fade: true,
+    cssEase: 'linear',
+    prevArrow: '<button class="slide-arrow prev-arrow"><img src="assets/img/icons/icon-arrow-left.svg" alt="" /></button>',
+    nextArrow: '<button class="slide-arrow next-arrow"><img src="assets/img/icons/icon-arrow-right.svg" alt="" /></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true
+        }
+      }
+    ]
+  });
+
+  $(document).scroll(function () {
+    var $nav = $("header.fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+
+  $(document).scroll(function () {
+    var $gototop = $(".gototop");
+    $gototop.toggleClass('scrolled', $(this).scrollTop() > $gototop.height());
+  });
+
+});
