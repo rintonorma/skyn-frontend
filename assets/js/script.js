7f4997429345c73333eed66841c8d0abd4e506e3
rintonorma <rinto456@gmail.com>
2022-05-09 17:05:36 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,4 +48,15 @@ $(document).ready(function () {
     $gototop.toggleClass('scrolled', $(this).scrollTop() > $gototop.height());
   });
 
+  $(".toggle-password").click(function() {
+
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    var input = $($(this).attr("toggle"));
+    if (input.attr("type") == "password") {
+      input.attr("type", "text");
+    } else {
+      input.attr("type", "password");
+    }
+  });
+
 });
